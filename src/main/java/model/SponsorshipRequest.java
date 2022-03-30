033026ec0ecd0497f73d23a7a3f104fe18298f23
Yanjin-Huang <Hortenshanghai2002@outlook.com>
2022-03-30 19:48:27 +0100

    SponsorshipRequest finished

--- src/main/java/model/SponsorshipRequest.java
+++ src/main/java/model/SponsorshipRequest.java
@@ -4,10 +4,43 @@ public class SponsorshipRequest {
     private long requestNumber;
     private TicketedEvent event;
     private SponsorshipStatus status;
+    private Integer sponsoredPricePercent;
+    private TicketedEvent attribute;
+    private String sponsorAccuntEmail;
 
     public SponsorshipRequest(long requestNumber, TicketedEvent event){
         this.requestNumber = requestNumber;
         this.event = event;
         this.status = SponsorshipStatus.PENDING;
+    }
+
+    public long getRequestNumber(){
+        return this.requestNumber;
+    }
+
+    public TicketedEvent getEvent(){
+        return this.event;
+    }
+
+    public SponsorshipStatus getStatus(){
+        return this.status;
+    }
+
+    public Integer getSponsoredPricePercent(){
+        return this.sponsoredPricePercent;
+    }
+
+    public String getSponsorAccuntEmail(){
+        return this.sponsorAccuntEmail;
+    }
+
+    public void accept(int percent, String sponsorAccuntEmail){
+        this.status = SponsorshipStatus.ACCEPTED;
+        this.sponsorAccuntEmail = sponsorAccuntEmail;
+        this.sponsoredPricePercent = percent;
+    }
+
+    public void reject(){
+        this.status = SponsorshipStatus.REJECTED;
     }
 }
