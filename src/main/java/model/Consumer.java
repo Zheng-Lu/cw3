12ee5d704cd43972a0645a1ed03c36d2fd6d6c79
WangFan24 <s2102597@ed.ac.uk>
2022-03-30 18:24:14 +0100

    users2

--- src/main/java/model/Consumer.java
+++ src/main/java/model/Consumer.java
@@ -1,4 +1,52 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Consumer {
+public class Consumer extends User{
+    private String name, phoneNumber;
+    private List<Booking> bookingList = new ArrayList<>();
+    private ConsumerPreferences preferences;
+
+    public Consumer(String name, String email, String phoneNumber, String password, String paymentAccountEmail){
+        super(email,password,paymentAccountEmail);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookingList.add(booking);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ConsumerPreferences getPreferences(){
+        return this.preferences;
+    }
+
+    public void setPreferences​(ConsumerPreferences preferences){
+        this.preferences = preferences;
+    }
+
+    public List<Booking> getBookings(){
+        return this.bookingList;
+    }
+
+    public void notify(String message){
+        System.out.print(message);
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setPhoneNumber(String newPhoneNumber){
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 }
--- src/main/java/model/EntertainmentProvider.java
+++ src/main/java/model/EntertainmentProvider.java
@@ -1,4 +1,84 @@
 package model;
+import external.EntertainmentProviderSystem;
 
-public class EntertainmentProvider {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntertainmentProvider extends User {
+
+    private String orgName, orgAddress, mainRepName, mainRepEmail;
+    private List<String>otherRepNames = new ArrayList<>();
+    private List<String>otherRepEmails = new ArrayList<>();
+    private List<Event>events = new ArrayList<>();
+
+
+    public EntertainmentProvider(String orgName,
+                                  String orgAddress,
+                                  String paymentAccountEmail,
+                                  String mainRepName,
+                                  String mainRepEmail,
+                                  String password,
+                                  List<String> otherRepNames,
+                                  List<String> otherRepEmails){
+
+        super(mainRepEmail,password,paymentAccountEmail);
+        this.orgName = orgName;
+        this.orgAddress = orgAddress;
+        this.mainRepName = mainRepName;
+        this.mainRepEmail = mainRepEmail;
+        this.otherRepNames = otherRepNames;
+        this.otherRepEmails = otherRepEmails;
+    }
+
+    public void addEvent(Event event){
+        this.events.add(event);
+    }
+
+    public String getOrgName(){
+        return this.orgName;
+    }
+
+    public void setOrgName(String orgName){
+        this.orgName = orgName;
+    }
+
+    public String getOrgAddress(){
+        return this.orgAddress;
+    }
+
+    public void setOrgAddress(String orgAddress){
+        this.orgAddress = orgAddress;
+    }
+
+    public List<Event> getEvents(){
+        return events;
+    }
+
+    public void setMainRepName(String mainRepName){
+        this.mainRepName = mainRepName;
+    }
+
+    public void setMainRepEmail(String mainRepEmail){
+        this.mainRepEmail = mainRepEmail;
+    }
+
+    public void setOtherRepNames(List<String> otherRepNames){
+        this.otherRepNames = otherRepNames;
+    }
+
+    public void setOtherRepEmails(List<String> otherRepEmails){
+        this.otherRepEmails = otherRepEmails;
+    }
+
+    public EntertainmentProviderSystem getProviderSystem(){
+        return new EntertainmentProviderSystem(){
+
+        };
+    }
+
+    @Override
+    public String toString(){
+        return  "";
+    }
+
 }
