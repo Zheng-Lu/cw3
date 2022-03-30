17c9144cbcec5bde274bf5756ccacc2036c395af
WangFan24 <s2102597@ed.ac.uk>
2022-03-30 17:06:00 +0100

    BCrypt

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,16 @@
 package model;
 
-public class User {
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public abstract class User extends Object{
+
+    private String email, password, paymentAccountEmail;
+
+    protected User(String email, String password, String paymentAccountEmail){
+        this.email = email;
+        this.password = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+        this.paymentAccountEmail = paymentAccountEmail;
+    }
+
+
 }
