e2e93457710685cac9eae822b214963b8d07e4c5
WangFan24 <s2102597@ed.ac.uk>
2022-03-30 17:25:39 +0100

    User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,13 +4,42 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public abstract class User extends Object{
 
-    private String email, password, paymentAccountEmail;
+    private String email, passwordHash, paymentAccountEmail;
 
     protected User(String email, String password, String paymentAccountEmail){
         this.email = email;
-        this.password = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+        this.passwordHash = BCrypt.withDefaults().hashToString(12, password.toCharArray());
         this.paymentAccountEmail = paymentAccountEmail;
     }
 
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public boolean checkPasswordMatch(String password){
+        return BCrypt.verifyer().verify(password.toCharArray(), this.passwordHash).verified;
+    }
+
+    public void updatePassword​(String newPassword){
+        this.passwordHash = BCrypt.withDefaults().hashToString(12, newPassword.toCharArray());
+    }
+
+    public String getPaymentAccountEmail(){
+        return this.paymentAccountEmail;
+    }
+
+    public void setPaymentAccountEmail​(String newPaymentAccountEmail){
+        this.paymentAccountEmail = newPaymentAccountEmail;
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 
 }
