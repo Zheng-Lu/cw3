33f4092d04aab3590177c80502bc1a5c35c11b8d
Yanjin-Huang <Hortenshanghai2002@outlook.com>
2022-04-01 06:52:02 +0100

    Logging package finished

--- src/main/java/logging/LogEntry.java
+++ src/main/java/logging/LogEntry.java
@@ -1,4 +1,27 @@
 package logging;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class LogEntry {
+    private String callername;
+    private String result;
+    private Map<String,String> additionalInfo;
+
+    public LogEntry(String callername, Object result, Map<String,Object> additionalInfo){
+        this.callername = callername;
+        this.result = result.toString();
+        this.additionalInfo = additionalInfo.entrySet().stream().collect(Collectors.toMap(
+                Map.Entry::getKey,
+                entry -> String.valueOf(entry.getValue())));
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 }
--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -1,4 +1,38 @@
 package logging;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Logger {
+    private Logger instance;
+    private List<LogEntry> log = new ArrayList<>();
+
+    private Logger(){
+    }
+
+    public Logger getInstance() {
+        if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void logAction(String callerName, Object result){
+        LogEntry logentry = new LogEntry(callerName,result,null);
+        this.log.add(logentry);
+    }
+
+    public void logAction(String callername, Object result, Map<String,Object> additionalInfo){
+        LogEntry logentry = new LogEntry(callername,result,additionalInfo);
+        this.log.add(logentry);
+    }
+
+    public List<LogEntry> getLog(){
+        return log;
+    }
+
+    public void clearLog(){
+        log.clear();
+    }
 }
