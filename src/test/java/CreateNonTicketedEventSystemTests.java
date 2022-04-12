094358168ecffc3440f568006431f33f68997930
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-12 15:47:48 +0100

    Create Non Ticketed Event Test Added. Cannot pass

---
+++ src/test/java/CreateNonTicketedEventSystemTests.java
@@ -0,0 +1,53 @@
+import command.*;
+import controller.Controller;
+import logging.Logger;
+import model.*;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CreateNonTicketedEventSystemTests {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---");
+    }
+
+    private static void registerBuskingProvider(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "No org",
+                "Leith Walk",
+                "a hat on the ground",
+                "the best musicican ever",
+                "busk@every.day",
+                "When they say 'you can't do this': Ding Dong! You are wrong!",
+                Collections.emptyList(),
+                Collections.emptyList()
+        ));
+    }
+
+    @Test
+    @DisplayName("Create Non Ticketed Event should work")
+    void canCreateNonTicketedEvent() {
+        Controller controller = new Controller();
+
+        registerBuskingProvider(controller);
+        CreateNonTicketedEventCommand eventCmd = new CreateNonTicketedEventCommand(
+                "Music for everyone!",
+                EventType.Music
+        );
+        controller.runCommand(eventCmd);
+        System.out.println(eventCmd.getResult());
+    }
+}
