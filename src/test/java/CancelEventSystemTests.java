3950c4376885353523cb203ea9d9b870895a2007
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-12 15:46:12 +0100

    Cancel Event Test Added. Cannot pass

---
+++ src/test/java/CancelEventSystemTests.java
@@ -0,0 +1,153 @@
+import command.*;
+import controller.Controller;
+import logging.Logger;
+import model.*;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CancelEventSystemTests {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---");
+    }
+
+    private static void registerOlympicsProvider(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mt. Everest",
+                "noreply@gmail.com",
+                "Secret Identity",
+                "anonymous@gmail.com",
+                "anonymous",
+                List.of("Unknown Actor", "Spy"),
+                List.of("unknown@gmail.com", "spy@gmail.com")
+        ));
+    }
+
+    private static void loginOlympicsProvider(Controller controller) {
+        controller.runCommand(new LoginCommand("anonymous@gmail.com", "anonymous"));
+    }
+
+    private static void createOlympicsProviderWith2Events(Controller controller) {
+        CreateTicketedEventCommand eventCmd1 = new CreateTicketedEventCommand(
+                "London Summer Olympics",
+                EventType.Sports,
+                123456,
+                25,
+                true
+        );
+        controller.runCommand(eventCmd1);
+        long eventNumber1 = eventCmd1.getResult();
+
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Wimbledon",
+                LocalDateTime.now().plusMonths(1),
+                LocalDateTime.now().plusMonths(1).plusHours(8),
+                List.of("Everyone in disc throw and 400m sprint"),
+                false,
+                true,
+                true,
+                3000,
+                3000
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Swimming arena",
+                LocalDateTime.now().plusMonths(1),
+                LocalDateTime.now().plusHours(8),
+                List.of("Everyone in swimming"),
+                true,
+                true,
+                false,
+                200,
+                300
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Wimbledon",
+                LocalDateTime.now().plusMonths(1).plusDays(1),
+                LocalDateTime.now().plusMonths(1).plusDays(1).plusHours(6),
+                List.of("Everyone in javelin throw and long jump"),
+                false,
+                true,
+                true,
+                3000,
+                3000
+        ));
+
+        CreateTicketedEventCommand eventCmd2 = new CreateTicketedEventCommand(
+                "Winter Olympics",
+                EventType.Sports,
+                40000,
+                400,
+                true
+        );
+        controller.runCommand(eventCmd2);
+        long eventNumber2 = eventCmd2.getResult();
+
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber2,
+                "The Alps",
+                LocalDateTime.now().plusYears(2).plusMonths(7),
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(3),
+                List.of("Everyone in slalom skiing"),
+                true,
+                true,
+                true,
+                4000,
+                10000
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber2,
+                "Somewhere else",
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(2),
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(4),
+                List.of("Everyone in ski jump"),
+                true,
+                true,
+                true,
+                4000,
+                10000
+        ));
+    }
+
+    private static void providerCancelFirstEvent(Controller controller) {
+        ListEventsCommand cmd = new ListEventsCommand(true, true);
+        controller.runCommand(cmd);
+        List<Event> events = cmd.getResult();
+        controller.runCommand(new CancelEventCommand(events.get(0).getEventNumber(), "Trololol"));
+    }
+
+    @Test
+    @DisplayName("Cancel Event should work")
+    void canCancelEvent() {
+        Controller controller = new Controller();
+
+        // Entertainment Provider Register
+        registerOlympicsProvider(controller);
+        controller.runCommand(new LogoutCommand());
+
+        // Login and create the event then Logout
+        loginOlympicsProvider(controller);
+        createOlympicsProviderWith2Events(controller);
+        controller.runCommand(new LogoutCommand());
+
+        // Login then cancel the event
+        loginOlympicsProvider(controller);
+        providerCancelFirstEvent(controller);
+    }
+}
