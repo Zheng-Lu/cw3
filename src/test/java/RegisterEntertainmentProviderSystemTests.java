0c6788060d770453a5082005f72c33f4be0bfdb9
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-12 16:16:48 +0100

    Entertainment Provider Registrations Tests Added. Passed. Need deeper test

---
+++ src/test/java/RegisterEntertainmentProviderSystemTests.java
@@ -0,0 +1,45 @@
+import command.*;
+import controller.Controller;
+import logging.Logger;
+import model.*;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegisterEntertainmentProviderSystemTests {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---");
+    }
+
+    @Test
+    @DisplayName("Entertainment Provider Registrations should work")
+    void successfulEntertainmentProviderRegistered(){
+        Controller controller = new Controller();
+
+//        register3Consumers(controller);
+        RegisterEntertainmentProviderCommand cmd = new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mt. Everest",
+                "noreply@gmail.com",
+                "Secret Identity",
+                "anonymous@gmail.com",
+                "anonymous",
+                List.of("Unknown Actor", "Spy"),
+                List.of("unknown@gmail.com", "spy@gmail.com")
+        );
+        controller.runCommand(cmd);
+    }
+}
