7ac3a1edb83b3221736e9e0b5d587bdf0ab6cfa3
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-13 14:48:57 +0100

    CreateNonTicketedEventSystemTests Passed. May needed deeper tests.

--- src/test/java/CreateNonTicketedEventSystemTests.java
+++ src/test/java/CreateNonTicketedEventSystemTests.java
@@ -24,6 +24,19 @@ public class CreateNonTicketedEventSystemTests {
         System.out.println("---");
     }
 
+    private static void registerCinemaProvider(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "Cinema Conglomerate",
+                "Global Office, International Space Station",
+                "$$$@there'sNoEmailValidation.wahey!",
+                "Mrs Representative",
+                "odeon@cineworld.com",
+                "F!ghT th3 R@Pture",
+                List.of("Dr Strangelove"),
+                List.of("we_dont_get_involved@cineworld.com")
+        ));
+    }
+
     private static void registerBuskingProvider(Controller controller) {
         controller.runCommand(new RegisterEntertainmentProviderCommand(
                 "No org",
@@ -37,17 +50,70 @@ public class CreateNonTicketedEventSystemTests {
         ));
     }
 
+    private static void loginCinemaProvider(Controller controller) {
+        controller.runCommand(new LoginCommand("odeon@cineworld.com", "F!ghT th3 R@Pture"));
+    }
+
+    private static void loginBuskingProvider(Controller controller) {
+        controller.runCommand(new LoginCommand("busk@every.day", "When they say 'you can't do this': Ding Dong! You are wrong!"));
+    }
+
     @Test
-    @DisplayName("Create Non Ticketed Event should work")
-    void canCreateNonTicketedEvent() {
+    @DisplayName("Create 1 Non Ticketed Event should work")
+    void testCreate1NonTicketedEvent() {
         Controller controller = new Controller();
 
         registerBuskingProvider(controller);
+        controller.runCommand(new LogoutCommand());
+        loginBuskingProvider(controller);
+
         CreateNonTicketedEventCommand eventCmd = new CreateNonTicketedEventCommand(
                 "Music for everyone!",
                 EventType.Music
         );
         controller.runCommand(eventCmd);
-        System.out.println(eventCmd.getResult());
+        long eventNumber = eventCmd.getResult();
+        assertEquals(1, eventNumber,
+                "Number of events should match the expectation");
+    }
+
+    @Test
+    @DisplayName("Create 2 Non Ticketed Events should work")
+    void testCreate2NonTicketedEvents() {
+        Controller controller = new Controller();
+
+        // Example 1
+        registerBuskingProvider(controller);
+        controller.runCommand(new LogoutCommand());
+        loginBuskingProvider(controller);
+
+        CreateNonTicketedEventCommand eventCmd1 = new CreateNonTicketedEventCommand(
+                "Music for everyone!",
+                EventType.Music
+        );
+        controller.runCommand(eventCmd1);
+        long eventNumber1 = eventCmd1.getResult();
+
+        controller.runCommand(new LogoutCommand());
+
+        // Example 2
+        registerCinemaProvider(controller);
+        controller.runCommand(new LogoutCommand());
+        loginCinemaProvider(controller);
+
+        CreateNonTicketedEventCommand eventCmd2 = new CreateNonTicketedEventCommand(
+                "The Shining at the Meadows (Free Screening) (Live Action)",
+                EventType.Sports
+        );
+        controller.runCommand(eventCmd2);
+        long eventNumber2 = eventCmd2.getResult();
+
+        controller.runCommand(new LogoutCommand());
+
+
+        assertEquals(1, eventNumber1,
+                "Number of events should match the expectation");
+        assertEquals(2, eventNumber2,
+                "Number of events should match the expectation");
     }
 }
