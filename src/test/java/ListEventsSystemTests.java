73f9768a7baff990fc15f3192fd7318864a613db
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-13 17:15:25 +0100

    command.ListEventBookingsCommand cannot pass

--- src/test/java/ListEventsSystemTests.java
+++ src/test/java/ListEventsSystemTests.java
@@ -24,14 +24,240 @@ public class ListEventsSystemTests {
         System.out.println("---");
     }
 
+    private static void createBuskingProviderWith1Event(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "No org",
+                "Leith Walk",
+                "a hat on the ground",
+                "the best musicican ever",
+                "busk@every.day",
+                "When they say 'you can't do this': Ding Dong! You are wrong!",
+                Collections.emptyList(),
+                Collections.emptyList()
+        ));
+
+        CreateNonTicketedEventCommand eventCmd = new CreateNonTicketedEventCommand(
+                "Music for everyone!",
+                EventType.Music
+        );
+        controller.runCommand(eventCmd);
+        long eventNumber = eventCmd.getResult();
+
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber,
+                "Leith as usual",
+                LocalDateTime.of(2030, 3, 20, 4, 20),
+                LocalDateTime.of(2030, 3, 20, 6, 45),
+                List.of("The same musician"),
+                true,
+                true,
+                true,
+                Integer.MAX_VALUE,
+                Integer.MAX_VALUE
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber,
+                "You know it",
+                LocalDateTime.of(2030, 3, 21, 4, 20),
+                LocalDateTime.of(2030, 3, 21, 7, 0),
+                List.of("The usual"),
+                true,
+                true,
+                true,
+                Integer.MAX_VALUE,
+                Integer.MAX_VALUE
+        ));
+
+        controller.runCommand(new LogoutCommand());
+    }
+
+    private static void createCinemaProviderWith3Events(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "Cinema Conglomerate",
+                "Global Office, International Space Station",
+                "$$$@there'sNoEmailValidation.wahey!",
+                "Mrs Representative",
+                "odeon@cineworld.com",
+                "F!ghT th3 R@Pture",
+                List.of("Dr Strangelove"),
+                List.of("we_dont_get_involved@cineworld.com")
+        ));
+
+        CreateTicketedEventCommand eventCmd1 = new CreateTicketedEventCommand(
+                "The LEGO Movie",
+                EventType.Movie,
+                50,
+                15.75,
+                false
+        );
+        controller.runCommand(eventCmd1);
+        long eventNumber1 = eventCmd1.getResult();
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "You know how much it hurts when you step on a Lego piece?!?!",
+                LocalDateTime.now().minusWeeks(1),
+                LocalDateTime.now().minusWeeks(1).plusHours(2),
+                Collections.emptyList(),
+                false,
+                true,
+                false,
+                50,
+                50
+        ));
+
+        CreateTicketedEventCommand eventCmd2 = new CreateTicketedEventCommand(
+                "Frozen Ballet",
+                EventType.Dance,
+                50,
+                35,
+                true
+        );
+        controller.runCommand(eventCmd2);
+        long eventNumber2 = eventCmd2.getResult();
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber2,
+                "Odeon cinema",
+                LocalDateTime.now().plusWeeks(1).plusDays(2).plusHours(3),
+                LocalDateTime.now().plusWeeks(1).plusDays(2).plusHours(6),
+                Collections.emptyList(),
+                false,
+                true,
+                false,
+                50,
+                50
+        ));
+
+        CreateNonTicketedEventCommand eventCmd3 = new CreateNonTicketedEventCommand(
+                "The Shining at the Meadows (Free Screening) (Live Action)",
+                EventType.Sports
+        );
+        controller.runCommand(eventCmd3);
+        long eventNumber3 = eventCmd3.getResult();
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber3,
+                "The Meadows, Edinburgh",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusYears(1),
+                List.of("You"),
+                false,
+                false,
+                true,
+                1000,
+                9999
+        ));
+
+        controller.runCommand(new LogoutCommand());
+    }
+
+    private static void createOlympicsProviderWith2Events(Controller controller) {
+        controller.runCommand(new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mt. Everest",
+                "noreply@gmail.com",
+                "Secret Identity",
+                "anonymous@gmail.com",
+                "anonymous",
+                List.of("Unknown Actor", "Spy"),
+                List.of("unknown@gmail.com", "spy@gmail.com")
+        ));
+
+        CreateTicketedEventCommand eventCmd1 = new CreateTicketedEventCommand(
+                "London Summer Olympics",
+                EventType.Sports,
+                123456,
+                25,
+                true
+        );
+        controller.runCommand(eventCmd1);
+        long eventNumber1 = eventCmd1.getResult();
+
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Wimbledon",
+                LocalDateTime.now().plusMonths(1),
+                LocalDateTime.now().plusMonths(1).plusHours(8),
+                List.of("Everyone in disc throw and 400m sprint"),
+                false,
+                true,
+                true,
+                3000,
+                3000
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Swimming arena",
+                LocalDateTime.now().plusMonths(1),
+                LocalDateTime.now().plusHours(8),
+                List.of("Everyone in swimming"),
+                true,
+                true,
+                false,
+                200,
+                300
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber1,
+                "Wimbledon",
+                LocalDateTime.now().plusMonths(1).plusDays(1),
+                LocalDateTime.now().plusMonths(1).plusDays(1).plusHours(6),
+                List.of("Everyone in javelin throw and long jump"),
+                false,
+                true,
+                true,
+                3000,
+                3000
+        ));
+
+        CreateTicketedEventCommand eventCmd2 = new CreateTicketedEventCommand(
+                "Winter Olympics",
+                EventType.Sports,
+                40000,
+                400,
+                true
+        );
+        controller.runCommand(eventCmd2);
+        long eventNumber2 = eventCmd2.getResult();
+
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber2,
+                "The Alps",
+                LocalDateTime.now().plusYears(2).plusMonths(7),
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(3),
+                List.of("Everyone in slalom skiing"),
+                true,
+                true,
+                true,
+                4000,
+                10000
+        ));
+        controller.runCommand(new AddEventPerformanceCommand(
+                eventNumber2,
+                "Somewhere else",
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(2),
+                LocalDateTime.now().plusYears(2).plusMonths(7).plusDays(4),
+                List.of("Everyone in ski jump"),
+                true,
+                true,
+                true,
+                4000,
+                10000
+        ));
+
+        controller.runCommand(new LogoutCommand());
+    }
+
     @Test
     @DisplayName("List Events should work")
     void testListEvents() {
         Controller controller = new Controller();
 
+        createOlympicsProviderWith2Events(controller);
+        createCinemaProviderWith3Events(controller);
+        createBuskingProviderWith1Event(controller);
+
         ListEventsCommand cmd = new ListEventsCommand(true, true);
         controller.runCommand(cmd);
         List<Event> events = cmd.getResult();
-        System.out.println(events);
+        assertEquals(6, events.size());
     }
 }
