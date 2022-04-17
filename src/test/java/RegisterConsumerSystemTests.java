2b03f62fa4c431a7d584d077581ca2a4f8f74978
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-17 14:28:18 +0100

    RegisterConsumerSystemTests updated. Logging tests failed.

--- src/test/java/RegisterConsumerSystemTests.java
+++ src/test/java/RegisterConsumerSystemTests.java
@@ -11,6 +11,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class RegisterConsumerSystemTests {
     @BeforeEach
@@ -24,51 +27,22 @@ public class RegisterConsumerSystemTests {
         System.out.println("---");
     }
 
-    private static void register3Consumers(Controller controller) {
-        controller.runCommand(new RegisterConsumerCommand(
-                "John Biggson",
-                "jbiggson1@hotmail.co.uk",
-                "077893153480",
-                "jbiggson2",
-                "jbiggson1@hotmail.co.uk"
-        ));
-        controller.runCommand(new LogoutCommand());
-        controller.runCommand(new RegisterConsumerCommand(
-                "Jane Giantsdottir",
-                "jane@inf.ed.ac.uk",
-                "04462187232",
-                "giantsRverycool",
-                "jane@aol.com"
-        ));
-        controller.runCommand(new LogoutCommand());
-        controller.runCommand(new RegisterConsumerCommand(
-                "Wednesday Kebede",
-                "i-will-kick-your@gmail.com",
-                "-",
-                "it is wednesday my dudes",
-                "i-will-kick-your@gmail.com"
-        ));
-        controller.runCommand(new LogoutCommand());
-    }
 
     @Test
     @DisplayName("Consumer Registrations should work")
     void successfulConsumerRegistered(){
         Controller controller = new Controller();
 
-//        register3Consumers(controller);
-
-        RegisterConsumerCommand cmd = new RegisterConsumerCommand(
+        RegisterConsumerCommand cmd1 = new RegisterConsumerCommand(
                 "John Biggson",
                 "jbiggson1@hotmail.co.uk",
                 "077893153480",
                 "jbiggson2",
                 "jbiggson1@hotmail.co.uk"
         );
-        controller.runCommand(cmd);
+        controller.runCommand(cmd1);
+        System.out.println(Logger.getInstance().getLog().get(Logger.getInstance().getLog().size()-1).getResult());
+        assertNull(Logger.getInstance().getLog().get(Logger.getInstance().getLog().size()-1).getResult());
 
-//        List<Consumer> consumers = cmd.getResult();
-//        assertEquals(1, consumers.size());
-//        assertTrue(consumers.stream().anyMatch(consumer -> consumer.getName().equals("John Biggson")));
     }
 }
