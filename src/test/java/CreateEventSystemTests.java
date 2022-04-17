7f909685cc378bb93fc488bdbb63e618a3ec8508
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-17 20:32:11 +0100

    CreateEvent System Tests Updated. All tests passed except some edge cases

--- src/test/java/CreateEventSystemTests.java
+++ src/test/java/CreateEventSystemTests.java
@@ -1,5 +1,6 @@
 import command.*;
 import controller.Controller;
+import logging.LogEntry;
 import logging.Logger;
 import model.*;
 import org.junit.jupiter.api.*;
@@ -52,6 +53,7 @@ public class CreateEventSystemTests {
                 List.of("Unknown Actor", "Spy"),
                 List.of("unknown@gmail.com", "spy@gmail.com")
         ));
+        controller.runCommand(new LogoutCommand());
     }
 
     private static void registerBuskingProvider(Controller controller) {
@@ -68,6 +70,10 @@ public class CreateEventSystemTests {
         controller.runCommand(new LogoutCommand());
     }
 
+    private static void loginOlympicsProvider(Controller controller) {
+        controller.runCommand(new LoginCommand("anonymous@gmail.com", "anonymous"));
+    }
+
     private static void loginCinemaProvider(Controller controller) {
         controller.runCommand(new LoginCommand("odeon@cineworld.com", "F!ghT th3 R@Pture"));
     }
@@ -75,4 +81,140 @@ public class CreateEventSystemTests {
     private static void loginBuskingProvider(Controller controller) {
         controller.runCommand(new LoginCommand("busk@every.day", "When they say 'you can't do this': Ding Dong! You are wrong!"));
     }
+
+    private String getLog() {
+        List<LogEntry> entries = Logger.getInstance().getLog();
+        String logStatus = entries.get(entries.size()-1).getAdditionalInfo().toString();
+        System.out.println(logStatus);
+        return logStatus;
+    }
+
+    @Test
+    @DisplayName("Creat Ticketed Events should work >")
+    void canCreateTicketedEvent() {
+        Controller controller = new Controller();
+
+        registerOlympicsProvider(controller);
+        loginOlympicsProvider(controller);
+        System.out.println("Creating Ticketed Event1 with requested Sponsorship:");
+        CreateTicketedEventCommand eventCmd1 = new CreateTicketedEventCommand(
+                "London Summer Olympics",
+                EventType.Sports,
+                123456,
+                25,
+                true
+        );
+        controller.runCommand(eventCmd1);
+        long numEvents1 = eventCmd1.getResult();
+        String tickedEventStatus1 = getLog();
+        assertEquals("{STATUS:=CREATE_EVENT_REQUESTED_SPONSORSHIP}", tickedEventStatus1);
+
+        System.out.println("Creating Ticketed Event2 without requested Sponsorship:");
+        CreateTicketedEventCommand eventCmd2 = new CreateTicketedEventCommand(
+                "Winter Olympics",
+                EventType.Sports,
+                40000,
+                400,
+                false
+        );
+        controller.runCommand(eventCmd2);
+        long numEvents2 = eventCmd2.getResult();
+        String tickedEventStatus2 = getLog();
+        assertEquals("{STATUS:=CREATE_TICKETED_EVENT_SUCCESS}", tickedEventStatus2);
+        controller.runCommand(new LogoutCommand());
+
+        registerCinemaProvider(controller);
+        loginCinemaProvider(controller);
+        System.out.println("Creating Ticketed Event3 without requested Sponsorship:");
+        CreateTicketedEventCommand eventCmd3 = new CreateTicketedEventCommand(
+                "The LEGO Movie",
+                EventType.Movie,
+                50,
+                15.75,
+                false
+        );
+        controller.runCommand(eventCmd3);
+        long numEvents3 = eventCmd3.getResult();
+        String tickedEventStatus3 = getLog();
+        assertEquals("{STATUS:=CREATE_TICKETED_EVENT_SUCCESS}", tickedEventStatus3);
+        controller.runCommand(new LogoutCommand());
+
+        assertEquals(1, numEvents1,
+                "Number of events should match the expectation");
+        assertEquals(2, numEvents2,
+                "Number of events should match the expectation");
+        assertEquals(3, numEvents3,
+                "Number of events should match the expectation");
+
+    }
+
+    @Test
+    @DisplayName("Creat Non-Ticketed Events should work >")
+    void canCreateNonTicketedEvent() {
+        Controller controller = new Controller();
+
+        registerBuskingProvider(controller);
+        loginBuskingProvider(controller);
+        System.out.println("Creating Non-Ticketed Event1:");
+        CreateNonTicketedEventCommand eventCmd1 = new CreateNonTicketedEventCommand(
+                "Music for everyone!",
+                EventType.Music
+        );
+        controller.runCommand(eventCmd1);
+        long numEvents1 = eventCmd1.getResult();
+        String tickedEventStatus1 = getLog();
+        assertEquals("{STATUS:=CREATE_NON_TICKETED_EVENT_SUCCESS}", tickedEventStatus1);
+        controller.runCommand(new LogoutCommand());
+
+        registerCinemaProvider(controller);
+        loginCinemaProvider(controller);
+        System.out.println("Creating Non-Ticketed Event2:");
+        CreateNonTicketedEventCommand eventCmd2 = new CreateNonTicketedEventCommand(
+                "The Shining at the Meadows (Free Screening) (Live Action)",
+                EventType.Sports
+        );
+        controller.runCommand(eventCmd2);
+        long numEvents2 = eventCmd2.getResult();
+        String tickedEventStatus2 = getLog();
+        assertEquals("{STATUS:=CREATE_NON_TICKETED_EVENT_SUCCESS}", tickedEventStatus2);
+        controller.runCommand(new LogoutCommand());
+
+        assertEquals(1, numEvents1,
+                "Number of events should match the expectation");
+        assertEquals(2, numEvents2,
+                "Number of events should match the expectation");
+    }
+
+//    @Test
+//    @DisplayName("Test Creating Events With Null Information >")
+//    void testCreateEventsWithNullInformation() {
+//        Controller controller = new Controller();
+//
+//        registerBuskingProvider(controller);
+//        loginBuskingProvider(controller);
+//        System.out.println("Creating Non-Ticketed Event1:");
+//        CreateNonTicketedEventCommand eventCmd1 = new CreateNonTicketedEventCommand(
+//                null,
+//                EventType.Music
+//        );
+//        controller.runCommand(eventCmd1);
+//        long numEvents1 = eventCmd1.getResult();
+//        controller.runCommand(new LogoutCommand());
+//
+//        registerCinemaProvider(controller);
+//        loginCinemaProvider(controller);
+//        System.out.println("Creating Non-Ticketed Event2:");
+//        CreateNonTicketedEventCommand eventCmd2 = new CreateNonTicketedEventCommand(
+//                "The Shining at the Meadows (Free Screening) (Live Action)",
+//                null
+//        );
+//        controller.runCommand(eventCmd2);
+//        long numEvents2 = eventCmd2.getResult();
+//        controller.runCommand(new LogoutCommand());
+//
+//        assertEquals(0, numEvents1,
+//                "Number of events should match the expectation");
+//        assertEquals(0, numEvents2,
+//                "Number of events should match the expectation");
+//    }
 }
