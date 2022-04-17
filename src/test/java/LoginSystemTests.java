77368e25fe7f1ac8c559024e2366c3cdf5febc1e
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-17 15:41:40 +0100

    LoginSystemTests updated. All tests passed.

--- src/test/java/LoginSystemTests.java
+++ src/test/java/LoginSystemTests.java
@@ -1,5 +1,6 @@
 import command.*;
 import controller.Controller;
+import logging.LogEntry;
 import logging.Logger;
 import model.*;
 import org.junit.jupiter.api.AfterEach;
@@ -93,6 +94,13 @@ public class LoginSystemTests {
         controller.runCommand(new LogoutCommand());
     }
 
+    String getLog() {
+        List<LogEntry> entries = Logger.getInstance().getLog();
+        String logStatus = entries.get(entries.size()-1).getAdditionalInfo().toString();
+        System.out.println(logStatus);
+        return logStatus;
+    }
+
 
     @Test
     @DisplayName("Consumer Login should work")
@@ -104,6 +112,8 @@ public class LoginSystemTests {
         // Login case 1
         LoginCommand login_cmd1 = new LoginCommand("jbiggson1@hotmail.co.uk", "jbiggson2");
         controller.runCommand(login_cmd1);
+        String loginStatus1 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus1);
         User consumers1 = login_cmd1.getResult();
         assertNotNull(consumers1);
         assertEquals("jbiggson1@hotmail.co.uk", consumers1.getEmail());
@@ -111,11 +121,15 @@ public class LoginSystemTests {
 
         LogoutCommand logout_cmd1 = new LogoutCommand();
         controller.runCommand(logout_cmd1);
+        String logoutStatus1 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus1);
         assertNull(logout_cmd1.getResult());
 
         // Login case 2
         LoginCommand login_cmd2 = new LoginCommand("jane@inf.ed.ac.uk", "giantsRverycool");
         controller.runCommand(login_cmd2);
+        String loginStatus2 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus2);
         User consumers2 = login_cmd2.getResult();
         assertNotNull(consumers2);
         assertEquals("jane@inf.ed.ac.uk", consumers2.getEmail());
@@ -123,11 +137,15 @@ public class LoginSystemTests {
 
         LogoutCommand logout_cmd2 = new LogoutCommand();
         controller.runCommand(logout_cmd2);
+        String logoutStatus2 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus2);
         assertNull(logout_cmd2.getResult());
 
         // Login case 3
         LoginCommand login_cmd3 = new LoginCommand("i-will-kick-your@gmail.com", "it is wednesday my dudes");
         controller.runCommand(login_cmd3);
+        String loginStatus3 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus3);
         User consumers3 = login_cmd3.getResult();
         assertNotNull(consumers3);
         assertEquals("i-will-kick-your@gmail.com", consumers3.getEmail());
@@ -135,6 +153,8 @@ public class LoginSystemTests {
 
         LogoutCommand logout_cmd3 = new LogoutCommand();
         controller.runCommand(logout_cmd3);
+        String logoutStatus3 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus3);
         assertNull(logout_cmd3.getResult());
     }
 
@@ -148,7 +168,8 @@ public class LoginSystemTests {
 
         LoginCommand login_cmd1 = new LoginCommand("busk@every.day", "When they say 'you can't do this': Ding Dong! You are wrong!");
         controller.runCommand(login_cmd1);
-        System.out.println(Logger.getInstance().getLog().get(Logger.getInstance().getLog().size()-1).getResult());
+        String loginStatus1 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus1);
         User entertainmentProvider1 = login_cmd1.getResult();
         assertNotNull(entertainmentProvider1);
         assertEquals("busk@every.day", entertainmentProvider1.getEmail());
@@ -156,10 +177,14 @@ public class LoginSystemTests {
         assertEquals("a hat on the ground", entertainmentProvider1.getPaymentAccountEmail());
         LogoutCommand logout_cmd1 = new LogoutCommand();
         controller.runCommand(logout_cmd1);
+        String logoutStatus1 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus1);
         assertNull(logout_cmd1.getResult());
 
         LoginCommand login_cmd2 = new LoginCommand("odeon@cineworld.com", "F!ghT th3 R@Pture");
         controller.runCommand(login_cmd2);
+        String loginStatus2 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus2);
         User entertainmentProvider2 = login_cmd2.getResult();
         assertNotNull(entertainmentProvider2);
         assertEquals("odeon@cineworld.com", entertainmentProvider2.getEmail());
@@ -167,10 +192,14 @@ public class LoginSystemTests {
         assertEquals("$$$@there'sNoEmailValidation.wahey!", entertainmentProvider2.getPaymentAccountEmail());
         LogoutCommand logout_cmd2 = new LogoutCommand();
         controller.runCommand(logout_cmd2);
+        String logoutStatus2 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus2);
         assertNull(logout_cmd2.getResult());
 
         LoginCommand login_cmd3 = new LoginCommand("anonymous@gmail.com", "anonymous");
         controller.runCommand(login_cmd3);
+        String loginStatus3 = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus3);
         User entertainmentProvider3 = login_cmd3.getResult();
         assertNotNull(entertainmentProvider3);
         assertEquals("anonymous@gmail.com", entertainmentProvider3.getEmail());
@@ -178,6 +207,8 @@ public class LoginSystemTests {
         assertEquals("noreply@gmail.com", entertainmentProvider3.getPaymentAccountEmail());
         LogoutCommand logout_cmd3 = new LogoutCommand();
         controller.runCommand(logout_cmd3);
+        String logoutStatus3 = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus3);
         assertNull(logout_cmd3.getResult());
     }
 
@@ -188,6 +219,8 @@ public class LoginSystemTests {
 
         LoginCommand login_cmd = new LoginCommand("margaret.thatcher@gov.uk", "The Good times  ");
         controller.runCommand(login_cmd);
+        String loginStatus = getLog();
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",loginStatus);
         User government = login_cmd.getResult();
         assertNotNull(government);
         assertEquals("margaret.thatcher@gov.uk", government.getEmail());
@@ -195,6 +228,8 @@ public class LoginSystemTests {
 
         LogoutCommand logout_cmd = new LogoutCommand();
         controller.runCommand(logout_cmd);
+        String logoutStatus = getLog();
+        assertEquals("{STATUS:=USER_LOGOUT_SUCCESS}",logoutStatus);
         assertNull(logout_cmd.getResult());
     }
 }
