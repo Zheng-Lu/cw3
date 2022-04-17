aefec9daaef470c0e8487ea8831a3cfa90f6f1f5
Zheng-Lu <s2085615@ed.ac.uk>
2022-04-17 16:42:52 +0100

    RegisterEntertainmentProvider System Tests updated. Some tests failed.

--- src/test/java/RegisterEntertainmentProviderSystemTests.java
+++ src/test/java/RegisterEntertainmentProviderSystemTests.java
@@ -24,12 +24,18 @@ public class RegisterEntertainmentProviderSystemTests {
         System.out.println("---");
     }
 
+    String getLog() {
+        List<LogEntry> entries = Logger.getInstance().getLog();
+        String logStatus = entries.get(entries.size()-1).getAdditionalInfo().toString();
+        System.out.println(logStatus);
+        return logStatus;
+    }
+
     @Test
     @DisplayName("Entertainment Provider Registrations should work")
     void successfulEntertainmentProviderRegistered(){
         Controller controller = new Controller();
 
-//        register3Consumers(controller);
         RegisterEntertainmentProviderCommand cmd = new RegisterEntertainmentProviderCommand(
                 "Olympics Committee",
                 "Mt. Everest",
@@ -41,8 +47,87 @@ public class RegisterEntertainmentProviderSystemTests {
                 List.of("unknown@gmail.com", "spy@gmail.com")
         );
         controller.runCommand(cmd);
-        List<LogEntry> logEntries = Logger.getInstance().getLog();
-        System.out.println(logEntries.get(logEntries.size()-1).getResult());
-//        assertNotNull(logEntries.get(logEntries.size()-1).getResult());
+        String logStatus = getLog();
+        assertNotNull(logStatus);
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",logStatus);
     }
+
+    @Test
+    @DisplayName("Entertainment Provider Registrations with existed organisation should not work")
+    void failedEntertainmentProviderRegisteredWithExistedOrg(){
+        Controller controller = new Controller();
+
+        RegisterEntertainmentProviderCommand cmd1 = new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mt. Everest",
+                "noreply@gmail.com",
+                "Secret Identity",
+                "anonymous@gmail.com",
+                "anonymous",
+                List.of("Unknown Actor", "Spy"),
+                List.of("unknown@gmail.com", "spy@gmail.com")
+        );
+        controller.runCommand(cmd1);
+        String logStatus1 = getLog();
+        assertNotNull(logStatus1);
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",logStatus1);
+
+        controller.runCommand(new LogoutCommand());
+
+        RegisterEntertainmentProviderCommand cmd2 = new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mr. Meeseek",
+                "nnnnnnnnnnoreply@gmail.com",
+                "Morty Smith",
+                "qc29m734yrc2q8ytrbn@gmail.com",
+                "c9pn8wqrwayansuh",
+                List.of("Unknown Spy", "Actor"),
+                List.of("Actor@gmail.com", "Unknown&Spy@gmail.com")
+        );
+
+        controller.runCommand(cmd2);
+        String logStatus2 = getLog();
+        assertNotNull(logStatus2);
+        assertEquals("{STATUS:=USER_REGISTER_ORG_ALREADY_REGISTERED}",logStatus2);
+    }
+
+    @Test
+    @DisplayName("Entertainment Provider Registrations with existed email should not work")
+    void failedEntertainmentProviderRegisteredWithExistedEmail(){
+        Controller controller = new Controller();
+
+        RegisterEntertainmentProviderCommand cmd1 = new RegisterEntertainmentProviderCommand(
+                "Olympics Committee",
+                "Mt. Everest",
+                "noreply@gmail.com",
+                "Secret Identity",
+                "anonymous@gmail.com",
+                "anonymous",
+                List.of("Unknown Actor", "Spy"),
+                List.of("unknown@gmail.com", "spy@gmail.com")
+        );
+        controller.runCommand(cmd1);
+        String logStatus1 = getLog();
+        assertNotNull(logStatus1);
+        assertEquals("{STATUS:=USER_LOGIN_SUCCESS}",logStatus1);
+
+        controller.runCommand(new LogoutCommand());
+
+        RegisterEntertainmentProviderCommand cmd2 = new RegisterEntertainmentProviderCommand(
+                "International Olympics Committee",
+                "Mr. Meeseek",
+                "nnnnnnnnnnoreply@gmail.com",
+                "Morty Smith",
+                "anonymous@gmail.com",
+                "c9pn8wqrwayansuh",
+                List.of("Unknown Spy", "Actor"),
+                List.of("Actor@gmail.com", "Unknown&Spy@gmail.com")
+        );
+
+        controller.runCommand(cmd2);
+        String logStatus2 = getLog();
+        assertNotNull(logStatus2);
+        assertEquals("{STATUS:=USER_REGISTER_EMAIL_ALREADY_REGISTERED}",logStatus2);
+    }
+
 }
